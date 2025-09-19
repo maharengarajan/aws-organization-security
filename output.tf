80d51c79fa799486c6ba0ce3f660eea9978e779b
maharengarajan <maharengarajan46@gmail.com>
2025-09-19 12:27:59 +0530

    outputs added

--- output.tf
+++ output.tf
@@ -10,3 +10,12 @@ output "account_a_id" {
 output "account_b_id" {
   value = aws_organizations_account.account_b.id
 }
+
+output "cloudtrail_logs_bucket" {
+  value = aws_s3_bucket.cloudtrail_logs.bucket
+}
+
+output "sns_topic_name" {
+  value = aws_sns_topic.security_monitoring_alerts.name
+  
+}
